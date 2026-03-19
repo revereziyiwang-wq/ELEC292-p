0810761c42d05cebc1c8e9acc660033a21d6ddf5
Tony Wang <23bf68@queensu.ca>
2026-03-19 00:26:57 -0400

    Added unzipped data files and updated main script

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import numpy as np
 import h5py
-
 
 
 def print_hi(name):
